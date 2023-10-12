8489887ae805d4ed19ab9effdd69b77c32121cf3
Cristofher <cristofherrojascr1@gmail.com>
2023-10-12 14:51:45 -0300

    se agrega logica para menu

--- app/src/main/java/com/example/menulateral/MainActivity.kt
+++ app/src/main/java/com/example/menulateral/MainActivity.kt
@@ -2,10 +2,63 @@ package com.example.menulateral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.menulateral.vistas.AboutFragment
+import com.example.menulateral.vistas.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //Menu barra izquierda superior
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        //Tolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment()).commit()
+                R.id.nav_data -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, AboutFragment()).commit()
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.syncState()
+        if(savedInstanceState==null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+
     }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+
 }
+
+
